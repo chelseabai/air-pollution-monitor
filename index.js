f35035509952a7c7bf8a8e54c7878dff04e04f30
chelseabai <xinyu.bai18@imperial.ac.uk>
2021-12-04 03:02:13 +0000

    reorganise file structure

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import routes from './routes/routes.js'
-import {postPollutionData} from "./controller/posts.js";
+import routes from './server/routes/routes.js'
+import {postPollutionData} from "./server/controller/posts.js";
 import path from 'path';
 
 const app = express();
@@ -15,10 +15,10 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
 
 const CONNECTION_URL = `mongodb+srv://chelseabai:happytina123@pollution-data.vssyr.mongodb.net/databaseA?retryWrites=true&w=majority`;
