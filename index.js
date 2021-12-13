397bf98fca1f46f59990456c69eb40ee3c30dfa3
chelseabai <xinyu.bai18@imperial.ac.uk>
2021-12-13 15:42:28 +0000

    updates

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import mqtt from 'mqtt';
+import url from "url";
 import cors from 'cors';
 import routes from './server/routes/routes.js'
 import {postPollutionData, postMQTTdata} from "./server/controller/posts.js";
@@ -27,27 +28,29 @@ app.get('/*', (req, res) => {
 const CONNECTION_URL = `mongodb+srv://chelseabai:happytina123@pollution-data.vssyr.mongodb.net/databaseA?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
-const client = mqtt.connect({ host: 'localhost', port: 1883 });
+const mqtt_url = process.env.CLOUDMQTT_URL || "driver.cloudmqtt.com:18982";
+const client = mqtt.connect(mqtt_url);
+
 const topic = 'esp/pm25';
 
 // Post pollution data from online API to the database
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, function (){
         console.log(`Server running on port: ${PORT}`);
-        // postPollutionData();
+        // client.on('connect', () => {
+        //     console.log('Connected');
+        //     client.subscribe([topic], () => {
+        //         console.log(`Subscribe to topic '${topic}'`);
+        //         client.on('message', (topic, payload) => {
+        //             postMQTTdata(topic, payload.toString());
+        //             console.log('Received Message:', topic, payload.toString());
+        //         });
+        //     })
+        // });
+
+// Subscribe to MQTT broker service to obtain data from sensor
+
+        postPollutionData();
     }))
     .catch((error) => console.log(error.message));
 
-client.on('connect', () => {
-    console.log('Connected');
-    client.subscribe([topic], () => {
-        console.log(`Subscribe to topic '${topic}'`)
-    })
-});
-console.log('hiii');
-
-// Subscribe to MQTT broker service to obtain data from sensor
-client.on('message', (topic, payload) => {
-    postMQTTdata(topic, payload.toString());
-    console.log('Received Message:', topic, payload.toString());
-});
