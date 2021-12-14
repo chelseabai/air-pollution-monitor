848b59b9cc4a5ea7286f49c17d4f686b49dd8268
chelseabai <xinyu.bai18@imperial.ac.uk>
2021-12-14 12:57:49 +0000

    add cors

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 const __dirname = path.resolve();
 
 app.use(express.static('frontend/build'));
